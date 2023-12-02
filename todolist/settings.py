ecfbeb1551c9e58e20f06fb69425d51e8ec53df5
megn <megn@example.com>
2023-12-02 13:44:24 +0000

    created the model and its migrations

--- todolist/settings.py
+++ todolist/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [ '*' ]
 # Application definition
 
 INSTALLED_APPS = [
+    "todoappv1.apps.Todoappv1Config",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
