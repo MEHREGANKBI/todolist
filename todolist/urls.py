33d9584d2b97e4bffcdcb624db5d1cff83e1a88e
megn@example.com <megn@example.com>
2023-12-12 02:19:35 +0330

    updated git so as to enable it to retrieve db data based on the url path received from the client

--- todolist/urls.py
+++ todolist/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("todoappv3/", include("todoappv3.urls"), name = "todoappv3urls"),
     path("todoappv2/", include("todoappv2.urls"), name = "todoappv2urls"),
     path("todoappv1/", include("todoappv1.urls"), name = "todoappv1urls"),
     path("admin/", admin.site.urls),
