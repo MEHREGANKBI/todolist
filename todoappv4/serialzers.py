2632082846137ee982d87596ba366906cb79c579
megn@example.com <megn@example.com>
2023-12-14 22:31:59 +0330

    initialized todoappv4, added it to root urls and settings

---
+++ todoappv4/serialzers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from .models import Todolist
+from .validators import *
+
+
+class TodolistSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Todolist
+        fields = [ 'id' , 'activity_description' , 'done_status']
+        read_only_fields = ['id']
+
+
+
+class GETTodolistSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Todolist
+        fields = ['id', 'activity_description']
+        read_only_fields = ['id', 'activity_description']
+
+
+class PUTTodolistSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required = True, min_value = 0,
+                                   validators = [todolist_id_exists_validator])
+    class Meta:
+        model = Todolist
+        fields = ['id','done_status']
+
+
+class DELETETodolistSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required = True, min_value = 0,
+                                   validators = [todolist_id_exists_validator])
+    class Meta:
+        model = Todolist
+        fields = ['id']
---
+++ todoappv4/validators.py
@@ -0,0 +1,9 @@
+from django.shortcuts import get_object_or_404
+from .models import Todolist
+from rest_framework import serializers
+
+def todolist_id_exists_validator(id):
+    try:
+        get_object_or_404(Todolist, id = id)
+    except:
+        raise serializers.ValidationError('The id is not valid.')
