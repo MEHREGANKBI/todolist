23ba3868082e64dff8d7033981762caf6772c10f
megn@example.com <megn@example.com>
2023-12-17 23:46:42 +0330

    added a max length parameter for every charfiled in every model. also migration files are now created

--- todoappv4/models.py
+++ todoappv4/models.py
@@ -4,25 +4,25 @@ from django.core import validators
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(null = False, blank = False, unique = True, validators = [
+    username = models.CharField(null = False, blank = False, unique = True, max_length = 12,validators = [
         validators.MinLengthValidator(4, message='Username must be at least 4 characters.'),
         validators.MaxLengthValidator(12, message='Username must be less than 12 characters.'),
         ])
-    password = models.CharField(null = False, blank = False)
+    password = models.CharField(null = False, blank = False, max_length = 256)
 
 
 
 class Tag(models.Model):
     
 
-    tag = models.CharField(null = False, blank = False, validators = [
+    tag = models.CharField(null = False, blank = False, max_length = 16, validators = [
         validators.MinLengthValidator(2, message='tag name must be at least 2 characters.'),
         validators.MaxLengthValidator(16, message='tag name must be less than 12 characters.'), 
         ])
     
 
 class Task(models.Model):
-    task = models.CharField(null = False, blank = False, validators= [
+    task = models.CharField(null = False, blank = False, max_length = 128, validators= [
         validators.MinLengthValidator(4, message= 'task description must be at least 4 characters.'),
         validators.MaxLengthValidator(128, message= 'task description must be less than 128 charatacters.')
     ])
--- todoappv4/serializers.py
+++ todoappv4/serializers.py
@@ -4,34 +4,34 @@ from rest_framework.validators import UniqueValidator
 from .validators import *
 
 
-class TodolistSerializer(serializers.ModelSerializer):
+# class TodolistSerializer(serializers.ModelSerializer):
     
-    class Meta:
-        model = Todolist
-        fields = [ 'id' , 'activity_description' , 'done_status']
-        read_only_fields = ['id']
+#     class Meta:
+#         model = Todolist
+#         fields = [ 'id' , 'activity_description' , 'done_status']
+#         read_only_fields = ['id']
 
 
 
-class GETTodolistSerializer(serializers.ModelSerializer):
+# class GETTodolistSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = Todolist
-        fields = ['id', 'activity_description']
-        read_only_fields = ['id', 'activity_description']
-
-
-class PUTTodolistSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(required = True, min_value = 0,
-                                   validators = [todolist_id_exists_validator])
-    class Meta:
-        model = Todolist
-        fields = ['id','done_status']
+#     class Meta:
+#         model = Todolist
+#         fields = ['id', 'activity_description']
+#         read_only_fields = ['id', 'activity_description']
 
 
-class DELETETodolistSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(required = True, min_value = 0,
-                                   validators = [todolist_id_exists_validator])
-    class Meta:
-        model = Todolist
-        fields = ['id']
+# class PUTTodolistSerializer(serializers.ModelSerializer):
+#     id = serializers.IntegerField(required = True, min_value = 0,
+#                                    validators = [todolist_id_exists_validator])
+#     class Meta:
+#         model = Todolist
+#         fields = ['id','done_status']
+
+
+# class DELETETodolistSerializer(serializers.ModelSerializer):
+#     id = serializers.IntegerField(required = True, min_value = 0,
+#                                    validators = [todolist_id_exists_validator])
+#     class Meta:
+#         model = Todolist
+#         fields = ['id']
