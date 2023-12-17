2a1de83f34b650742e3c88e9cfd696990b40be03
megn@example.com <megn@example.com>
2023-12-17 23:38:51 +0330

    created the models for the v4 app. also the signup and sigin url paths were added to v4

--- todoappv4/models.py
+++ todoappv4/models.py
@@ -1,9 +1,37 @@
 from django.db import models
+from django.core import validators
+
 
 # Create your models here.
-class Todolist(models.Model):
-    # null=False, blank = False, 
-    activity_description = models.CharField(max_length=200, unique = True)
-    done_status = models.BooleanField(blank= False ,null= False)
+class User(models.Model):
+    username = models.CharField(null = False, blank = False, unique = True, validators = [
+        validators.MinLengthValidator(4, message='Username must be at least 4 characters.'),
+        validators.MaxLengthValidator(12, message='Username must be less than 12 characters.'),
+        ])
+    password = models.CharField(null = False, blank = False)
 
 
+
+class Tag(models.Model):
+    
+
+    tag = models.CharField(null = False, blank = False, validators = [
+        validators.MinLengthValidator(2, message='tag name must be at least 2 characters.'),
+        validators.MaxLengthValidator(16, message='tag name must be less than 12 characters.'), 
+        ])
+    
+
+class Task(models.Model):
+    task = models.CharField(null = False, blank = False, validators= [
+        validators.MinLengthValidator(4, message= 'task description must be at least 4 characters.'),
+        validators.MaxLengthValidator(128, message= 'task description must be less than 128 charatacters.')
+    ])
+
+    is_complete = models.BooleanField(null = False, blank = False)
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    modified_at = models.DateTimeField(auto_now = True)
+    deadline_at = models.DateTimeField(null = False, blank = False)
+
+    Tag_id = models.ForeignKey(Tag, models.SET_NULL, null = True, blank = True)
+    User_id = models.ForeignKey(User, models.CASCADE, null = False, blank = False)
