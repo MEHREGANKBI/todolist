2791810b5b45100942ca2e603b258aaec19f0a6e
megn@example.com <megn@example.com>
2023-12-26 01:12:16 +0330

    sign up now uses the default django user model. password validation is in place and the whole process is handled by a single serializer

--- todoappv5/models.py
+++ todoappv5/models.py
@@ -1,20 +1,9 @@
 from django.db import models
 from django.core import validators
-
-
-# Create your models here.
-class User(models.Model):
-    username = models.CharField(null = False, blank = False, unique = True, max_length = 12,validators = [
-        validators.MinLengthValidator(4, message='Username must be at least 4 characters.'),
-        validators.MaxLengthValidator(12, message='Username must be less than 12 characters.'),
-        ])
-    password = models.CharField(null = False, blank = False, max_length = 256)
-
+from django.contrib.auth import get_user_model
 
 
 class Tag(models.Model):
-    
-
     tag = models.CharField(null = False, blank = False, unique = True, max_length = 16, validators = [
         validators.MinLengthValidator(2, message='tag name must be at least 2 characters.'),
         validators.MaxLengthValidator(16, message='tag name must be less than 12 characters.'), 
@@ -33,5 +22,5 @@ class Task(models.Model):
     modified_at = models.DateTimeField(auto_now = True)
     deadline_at = models.DateTimeField(null = False, blank = False)
 
-    Tag_id = models.ForeignKey(Tag, models.SET_NULL, null = True, blank = True)
-    User_id = models.ForeignKey(User, models.CASCADE, null = False, blank = False)
+    Tag = models.ForeignKey(Tag, models.SET_NULL, null = True, blank = True) # creates Tag and Tag_id orm fields.
+    User = models.ForeignKey(get_user_model(), models.CASCADE, null = False, blank = False)
