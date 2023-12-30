41c263a988750a8b2b07083d7a2d97f28e1dc7de
megn@example.com <megn@example.com>
2023-12-31 00:13:52 +0330

    moved some django settings from the settings.py file to .env file.

--- src/todolist/settings.py
+++ src/todolist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+_f2r87vbci&!n8rh*(%*luj*6(e*0d#v$u@9gzw8+v^8od&7w"
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = [ '*' ]
+ALLOWED_HOSTS = [ host.strip() for host in getenv('DJANGO_ALLOWED_HOSTS').split(',') ]
 
 
 # Application definition
