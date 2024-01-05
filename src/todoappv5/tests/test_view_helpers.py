a71d6a1097ae76adefbb6e83f5866c083ebb7fbc
mehregan <mehregank_9819@hotmail.com>
2024-01-05 22:33:07 +0330

    Multiple changes: 1: __init__.py was added to the tests folder to turn it into a package recognized by django 2: A comprehensive docstring was added to the <tag_exists> function in view_helpers.py 3: Initial testcases for the <tag_exists> function were added.

--- src/todoappv5/tests/test_view_helpers.py
+++ src/todoappv5/tests/test_view_helpers.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from todoappv5.models import *
 
+
+from todoappv5.view_helpers import *
 # Create your tests here.
+
+class TestTagExists(TestCase):
+    '''
+    This class tests two major cases, a tag that exsists and a tag that doesn't. 
+    Invalid tags will not be the subject of this test class since this function expects its sole parameter to be
+    sanitized. Tests regarding the invalid tags are out of scope for this class and are related to the correspoding serializer.
+
+    For every type of test in this class, there will be a minimum of 3 test cases that'll be run to make sure
+    the first or second one wasn't a fluke. 
+    '''
+
+    def setUp(self):
+        # These tags will exist in the tables and the test cases will be compared against them.
+        Tag.objects.create(tag = 'School')
+        Tag.objects.create(tag = 'Work')
+        Tag.objects.create(tag = 'Chores')
+
+    
+    def tearDown(self):
+        # This function will remove all records from the <Tag> table. 
+        Tag.objects.all().delete()
+
+    def test_existing_tags(self):
+        tag1 = 'School'
+        tag2 = 'Chores'
+        tag3 = 'Work'
+        self.assertTrue(tag_exists(tag= tag1))
+        self.assertTrue(tag_exists(tag= tag2))
+        self.assertTrue(tag_exists(tag= tag3))
