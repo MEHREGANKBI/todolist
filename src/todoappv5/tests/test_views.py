bb93eada434621992cd3e1dd18204f33a6b7fdca
mehregan <mehregank_9819@hotmail.com>
2024-01-07 02:02:36 +0330

    Created a new file for view tests. and made an initial structure of the test class for the GET method of the TaskView.

---
+++ src/todoappv5/tests/test_views.py
@@ -0,0 +1,18 @@
+from django.test import Client, TestCase
+
+class TestTaskViewGET(TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    def test_valid_params(self):
+        pass
+    def test_invalid_params(self):
+        pass
+    def test_unauthorized_user(self):
+        pass
+    def test_user_with_no_tasks(self):
+        pass
+    def test_block_listed_user(self):
+        pass
+    
