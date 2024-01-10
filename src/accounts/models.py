e2eb6dbe76b85d2002ff66da5726902d5c54816f
mehregan <mehregank_9819@hotmail.com>
2024-01-11 02:28:00 +0330

    Fixed the token model inheriting from the wrong class.

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class TokenBlockList(models):
+class TokenBlockList(models.Model):
     # Intentionally avoiding the unique constraint. It's unlikely for duplicate records to be made
     # and if they are made, we don't care as long as there aren't too many of them.
     # However, if we set the unique constraint, it needs to check the new token against every single
     # time a token is added i.e almost everytime someone logs out. Yikes. 
-    token = models.CharField(max_length = 8192, null = False, required = True)
+    token = models.CharField(max_length = 8192, null = False, blank = False)
