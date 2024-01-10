0ab9c0c4b92b09601a4677a5463bd2062535d72e
mehregan <mehregank_9819@hotmail.com>
2024-01-11 02:03:12 +0330

    added the logout view to the urlconf of the accounts app.

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("signin/", TokenObtainPairView.as_view(), name= "sign_in_view"),
     path("tokenRefresh/", TokenRefreshView.as_view(), name= "token_refresh_view"),
     path("signup/", views.SignupView.as_view(), name= "sign_up_view"),
+    path("logout/", views.LogOutView.as_view(), name="log_out_view"),
 ]
