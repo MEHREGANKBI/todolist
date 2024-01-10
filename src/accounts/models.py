33c3c4f8921e9e0a08a45b14b88315c19a51a4f3
mehregan <mehregank_9819@hotmail.com>
2024-01-11 02:25:07 +0330

    made a model for the token blocklist.

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class TokenBlockList(models):
+    # Intentionally avoiding the unique constraint. It's unlikely for duplicate records to be made
+    # and if they are made, we don't care as long as there aren't too many of them.
+    # However, if we set the unique constraint, it needs to check the new token against every single
+    # time a token is added i.e almost everytime someone logs out. Yikes. 
+    token = models.CharField(max_length = 8192, null = False, required = True)
