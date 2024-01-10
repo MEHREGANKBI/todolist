53b672244859d8c809b1bd2753e0cb98f825f3c8
mehregan <mehregank_9819@hotmail.com>
2024-01-11 02:50:21 +0330

    renamed the token filed from the TokenBlockList model to refresh_token, since we'll receive to tokens and <token> as the field name doesn't cut it.

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -5,4 +5,4 @@ class TokenBlockList(models.Model):
     # and if they are made, we don't care as long as there aren't too many of them.
     # However, if we set the unique constraint, it needs to check the new token against every single
     # time a token is added i.e almost everytime someone logs out. Yikes. 
-    token = models.CharField(max_length = 8192, null = False, blank = False)
+    refresh_token = models.CharField(max_length = 8192, null = False, blank = False)
