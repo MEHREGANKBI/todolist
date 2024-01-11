76d611b3786b177ffae0ab24010d9a3264753eb5
mehregan <mehregank_9819@hotmail.com>
2024-01-11 23:49:21 +0330

    Chnaged the urlconf so that the <tokenrefresh/> endpoint now points to my wrapper view rather than simplejwt's defaultt refresh view.

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -7,7 +7,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path("signin/", TokenObtainPairView.as_view(), name= "sign_in_view"),
-    path("tokenRefresh/", TokenRefreshView.as_view(), name= "token_refresh_view"),
+    path("tokenRefresh/", views.RefreshTokenWrapper.as_view(), name= "token_refresh_wrapper_view"),
     path("signup/", views.SignupView.as_view(), name= "sign_up_view"),
     path("logout/", views.LogOutView.as_view(), name="log_out_view"),
 ]
