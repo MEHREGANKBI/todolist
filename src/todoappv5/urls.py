96f67f1618fcd03d44b2b12e27323424a19f9f6d
mehregan <mehregank_9819@hotmail.com>
2024-01-14 02:35:50 +0330

    Changed the path of the <get> method in TaskView so that it no longer accepts path arguments but rather, query parameters.

--- src/todoappv5/urls.py
+++ src/todoappv5/urls.py
@@ -7,6 +7,6 @@ app_name="todoappv5"
 urlpatterns = [
     path("", views.TaskView.as_view(), name="root"),
     # # The following paths must only be used with their respective functions i.e get and delete, respectively.
-    path("get/<str:type_param>/", views.TaskView.as_view(), name = "retrieval_view"),
+    path("get/", views.TaskView.as_view(), name = "retrieval_view"),
     path("delete/<int:id_param>/", views.TaskView.as_view(), name = "delete_view"),
     ]
