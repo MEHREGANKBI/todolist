824421363e0a37145fef026ca2dccd3dd80d2ea4
mehregan <mehregank_9819@hotmail.com>
2024-01-15 16:55:38 +0330

    Added a docstring for the <make_success_response> function of the response factory module. Furthermore enhanced its type declarations and return value.

--- src/response_factory/default_responses.py
+++ src/response_factory/default_responses.py
@@ -1,9 +1,25 @@
 from rest_framework.status import HTTP_200_OK
-def make_success_response(result, message = 'SUCCESS...') -> tuple[dict,int] :
+
+
+
+def make_success_response(result, message : str = 'SUCCESS...') -> tuple[dict,int] :
+    '''
+    Parameters: 
+        result: The payload of the message. 
+        message: The message of the response. Currently sends a default text and doesn't provide any extra information.
+
+    Returns:
+        response_table: a tuple of [dict, int]. The int represents the HTTP request status. The dict is the response body.
+
+    Decription:
+        Given a payload and a message corresponding to a successful HTTP request, return a tuple of
+        the response body and the HTTP request status.
+    '''
     response_status = HTTP_200_OK
     response_dict = {}
 
     response_dict['message'] = message
     response_dict['result'] = result
 
-    return response_dict, response_status
+    response_tuple = response_dict, response_status
+    return response_tuple
