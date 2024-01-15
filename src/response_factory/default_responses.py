5433bb44cc4ef5034e1a0318648b90e62aa341e3
mehregan <mehregank_9819@hotmail.com>
2024-01-15 16:45:25 +0330

    Completed the logic for the <make_success_response> of the response factory module.

--- src/response_factory/default_responses.py
+++ src/response_factory/default_responses.py
@@ -1,3 +1,9 @@
 from rest_framework.status import HTTP_200_OK
 def make_success_response(result, message = 'SUCCESS...') -> tuple[dict,int] :
-    pass
+    response_status = HTTP_200_OK
+    response_dict = {}
+
+    response_dict['message'] = message
+    response_dict['result'] = result
+
+    return response_dict, response_status
