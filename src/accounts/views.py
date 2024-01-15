bcd23b09e96ec566ad24f16e69dbc5ec3b0a577a
mehregan <mehregank_9819@hotmail.com>
2024-01-15 17:51:25 +0330

    Collapsed the two methods of <signupview> into one post method that also makes use of the <make_success_response> function.

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -11,30 +11,17 @@ from response_factory.default_responses import make_success_response
 
 class SignupView(APIView):
 
+    def post(self,request):
 
-    def signup(self, request):
         deserialized_data = UserCreationSerializer(data= request.data)
 
         if deserialized_data.is_valid():
             deserialized_data.save()
 
-            response_status = status.HTTP_200_OK
-            response_message = 'SUCCESS...'
-            response_result = 'Your account was created. You can sign in now.'
+            response_dict, response_status = make_success_response(result='Your account was created. You can sign in now.')
 
         else:
             raise ParseError(deserialized_data.errors.__str__())
-
-        return response_message, response_result, response_status
-
-
-
-
-    def post(self,request):
-        response_status = None
-        response_dict = {}
-        
-        response_dict['message'], response_dict['result'], response_status = self.signup(request)  
 
         return JsonResponse(response_dict, safe= False, status = response_status)
     
